db59ad3c3457d55415914bd63e1c802830d333cc
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2023-12-17 00:42:38 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Chess Titans!");
+		Position pos= new Position(1,3);
+		System.out.println(pos);
 	}
 
 }
